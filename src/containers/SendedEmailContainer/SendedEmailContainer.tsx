6761eab665a6ee552bf1fe9445a46f4023816b8f
Diego Rojas <devup2332@gmail.com>
2023-01-13 17:38:58 -0500

    dev:  Added responsive estyles for SendedEmailContainer file

--- src/containers/SendedEmailContainer/SendedEmailContainer.tsx
+++ src/containers/SendedEmailContainer/SendedEmailContainer.tsx
@@ -6,17 +6,26 @@ import SendedEmailVector from "../../components/atoms/vectors/SendedEmailVector"
 const SendedEmailContainer = () => {
 	const { t } = useTranslation("index");
 	return (
-		<div className="flex justify-center text-white items-center h-screen bg-primary">
-			<div className="w-8/12 grid max-w-md border-2 border-red-400 gap-6">
-				<div className="grid gap-6">
-					<h1 className="text-center text-2xl">{t("sendedMessage.title")} </h1>
+		<div className="flex text-white items-center h-screen justify-center">
+			<div className="flex justify-self-center justify-center lg:justify-between w-10/12 lg:items-center lg:max-w-8xl">
+				<div className="grid gap-6 border-2 border-red-400 h-fit xl:gap-10">
+					<h1 className="text-center text-2xl font-extrabold lg:text-left xl:text-4xl 2xl:text-6xl">
+						{t("sendedMessage.title")}{" "}
+					</h1>
+					<SendedEmailVector className="w-full h-full lg:hidden" />
+					<p className="text-center leading-6 font-bold lg:text-left lg:font-normal">
+						{t("sendedMessage.description")}
+					</p>
 					<Link href="/">
-						<Button color="secondary" variant="contained" className="w-full">
+						<Button
+							color="secondary"
+							variant="contained"
+							className="w-full h-fit lg:w-40 xl:h-12">
 							{t("sendedMessage.button")}
 						</Button>
 					</Link>
 				</div>
-				<SendedEmailVector className="w-full h-full" />
+				<SendedEmailVector className="w-6/12 h-full hidden lg:block" />
 			</div>
 		</div>
 	);
